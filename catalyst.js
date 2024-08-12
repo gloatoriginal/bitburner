cd77b632a023b9af1ea4c29acae8569df4e6202b
Devin Baughman <47373236+gloatoriginal@users.noreply.github.com>
2024-08-12 12:29:59 -0700

    Update catalyst.js

--- catalyst.js
+++ catalyst.js
@@ -1,6 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  ns.killall()
+  //ns.killall()
   while (true) {
     let watchdog_config = await get_config(ns, 'catalyst.txt')
     let programs = await get_config(ns, 'programs.txt')
@@ -11,8 +11,8 @@ export async function main(ns) {
       if (!program.name.includes('.js')) program.name += '.js'
       if (program.enabled == 1) await async_run(ns, program)
     }
-
     await kill_rogue_procs(ns, programs, disabled_apps)
+
     await ns.sleep(watchdog_config.timeout)
   }
 }
@@ -23,21 +23,21 @@ async function get_config(ns, filename) {
 
 
 async function async_run(ns, program) {
-  if (!await program_running(ns, program.name)) {
+  let pr = await program_running(ns, program.name)
+  if (!pr) {
     try {
       ns.print('Starting: ' + program.name)
       ns.run(program.name, program.threads)
     } catch {
-      await error_log(ns, program)
+      await error_log(ns, [program.name, program.threads, program.description])
+      ns.run(program.name, 1)
     }
   }
 }
 
-async function error_log(ns, program) {
-  ns.print('WARNING, ERROR RUNNING PROGRAM:')
-  ns.print('Program Name: ' + program.name)
-  ns.print('Program Threads: ' + program.threads)
-  ns.print('Program Description: ' + program.description)
+async function error_log(ns, arg_array) {
+  ns.print('Error Log: ')
+  if (arg_array.length > 0) for (let arg of arg_array) ns.print(arg)
 }
 
 async function program_running(ns, name) {
@@ -51,28 +51,24 @@ async function program_running(ns, name) {
 
 async function kill_rogue_procs(ns, programs) {
   let ps_array = await ns.ps();
-  let pid_kill_list = []
-  // ns.print('Killing rogue procs and disabled ones')
-  for (let proc of ps_array) {
-    //ns.print(proc)
-    for (let prog of programs) {
-      // let prog = programs[j]
-      let current_script = await ns.getScriptName()
-      if (proc.filename != current_script && proc.filename == prog.name && prog.enabled == 0) {
-        await add_kill_list(ns, pid_kill_list, proc.filename, proc.pid) 
-      }
-    }
+  let disabled = []
+  let enabled = []
+  for(let prog of programs) {
+    if(prog.enabled == 0) disabled.push(prog.name)
+    else enabled.push(prog.name)
   }
-  for (let i of pid_kill_list) {
-    ns.print('Killed pid: ' + i)
-    await ns.kill(i)
+  for (let proc of ps_array){
+    if(disabled.includes(proc.filename) ||
+    !enabled.includes(proc.filename)) await kp(ns, proc.pid)
   }
 }
 
 
-async function add_kill_list(ns, pid_kill_list, filename, procid){
-  ns.print('Killing process: ' + filename)
-  pid_kill_list.push(procid)
+let kp_array = async (ns, procids) => {
+  for (let procid of procids) await kp(ns, procid)
 }
 
-
+// General safety net to not kill itself
+async function kp(ns, procid) {
+  if (ns.pid != procid) await ns.kill(procid)
+}
